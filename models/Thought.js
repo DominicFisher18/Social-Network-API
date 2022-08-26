36cc87b85b0e5db4056afaf01cff2afe30fb4910
DomDom <dominicfisher182001@gmail.com>
2022-08-26 19:25:30 -0400

    finished models

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,36 @@
 const { Schema, model } = require ('mongoose');
+const reactionScehma = require('./Reaction');
 
 const userScehma = new Schema (
     {
-        
+        thoughtText: {
+            type: String,
+            required: true,
+            maxLength: 280 
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: timeStr => new Date(timeStr).toDateString()
+        },
+        username: {
+            type: String,
+            requied: true
+        },
+         reactions: [reactionScehma],
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
     }
-)
+);
+
+userScehma.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const User = model('User', userScehma);
+
+module.exports = userScehma;
